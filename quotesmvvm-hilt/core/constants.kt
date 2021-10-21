db2f65a31151ee0025a1d2b6e3516aa0b9213001
Hermas386 <75684398+Hermas386@users.noreply.github.com>
2021-10-21 14:36:19 -0500

    Add files via upload

---
+++ quotesmvvm-hilt/core/constants.kt
@@ -0,0 +1,6 @@
+package dev.cardoso.quotesmvvm.core
+
+//const val BASE_URL= "http://10.0.0.10"
+const val BASE_URL = "https://raw.githubusercontent.com/jomc-hash/mvc-usuarios/master/"
+//const val API_PATH="/quotes.php"
+const val API_PATH="quotes"
